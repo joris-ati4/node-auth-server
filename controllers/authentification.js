e0fbd46d0043fe818469a718243d3be04743e4d7
Joris Hart <jhart@parkour3.com>
2017-12-28 14:47:36 -0500

    Inital commit on the NodeJS boilerplate auth server

---
+++ controllers/authentification.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+
+function tokenForUser(user) {
+	const timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next){
+	// User has already had their email and password auth'd
+	// we just need to give them a token
+	res.json({ token: tokenForUser(req.user) });
+}
+
+exports.signup = function(req, res, next){
+
+	const email = req.body.email;
+	const password = req.body.password;
+
+	if( !email || !password ){
+		return res.status(422).send({error: "You must provide a email and password"});
+	}
+
+	// See if a user with the given email exists
+	User.findOne({ email: email }, function(err, existingUser){
+		if(err) { return next(err); }
+
+		// If a user with ameil does exist, return an error
+		if( existingUser ){
+			return res.status(422).send({ error: 'Email is in use'});
+		}
+
+		// If a user with email does NOT exist, create and save user record
+		const user = new User({
+			email: email,
+			password: password
+		});
+
+		user.save( function(err){
+			if(err) { return next(err); }
+		});
+
+		//respond to request indicating the user was created
+		res.json({ token: tokenForUser(user) });
+	});
+}
---
+++ router.js
@@ -0,0 +1,13 @@
+const Authentification = require('./controllers/authentification')
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', {session: false});
+module.exports = function(app){
+	app.get('/', requireAuth, function( req, res){
+		res.send({hi: 'there'});
+	});
+	app.post('/signin', requireSignin, Authentification.signin);
+	app.post('/signup', Authentification.signup);
+}
